6c288724c7ecde509536c6d76081df432d1a342e
Mario Mancilla <mariormt17@gmail.com>
2021-04-13 18:15:08 -0500

    Create the function to find the jazz albums from the 50

--- get-csv.rb
+++ get-csv.rb
@@ -1,4 +1,10 @@
 require "csv"
 
+def classic_jazz?(album)
+  album[2] == "Jazz" and album[3].to_i.beetwen?(1950, 1959)
+end
+
 albums = CSV.read("albums.csv")[1..]
-print albums
+
+# Find jazz albums from the 50's
+
