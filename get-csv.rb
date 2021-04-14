0b41524be709ce2a213ffdee816ca10606d7aaeb
Mario Mancilla <mariormt17@gmail.com>
2021-04-13 23:02:43 -0500

    Code refactoring for better readability

--- get-csv.rb
+++ get-csv.rb
@@ -1,47 +1,61 @@
 require "csv"
 
-albums = CSV.read("albums.csv")[1..]
+AUTHOR = 1
+GENRE = 2
+YEAR = 3
 
-def classic_jazz?(album)
-  album[2] == "Jazz" and album[3].to_i.between?(1950, 1959)
-end
-
-def classic_pop?(album)
-  album[2] == "Pop" and album[3].to_i.between?(1980, 1989)
-end
-
-def rock_album?(album)
-  album[2] == "Rock"
-end
-
-def from_michael_jackson?(album)
-  album[1] == "Michael Jackson"
-end
-
-classic_jazz_albums = []
 # Find jazz albums from the 50's
-albums.each do |album|
-  classic_jazz_albums << album if classic_jazz?(album)
-end
-puts "Jazz 50's albums: #{classic_jazz_albums}"
+puts "------------------------------------------------"
+puts "Jazz albums from the 50's"
+CSV.read("albums.csv")[1..]
+  .select do |album|
+    album[GENRE] == "Jazz" and album[YEAR].to_i.between?(1950, 1959)
+  end
+  .sort_by do |album|
+    album[YEAR].to_i
+  end
+  .each do |album|
+    puts album.join(',')
+  end
 
-classic_pop_albums = []
 # Find eighties pop albums
-albums.each do |album|
-  classic_pop_albums << album if classic_pop?(album)
-end
-puts "Eighties pop albums: #{classic_pop_albums}"
+puts "------------------------------------------------"
+puts "Pop albums from the 80's"
+CSV.read("albums.csv")[1..]
+  .select do |album|
+    album[GENRE] == "Pop" and album[YEAR].to_i.between?(1980, 1989)
+  end
+  .sort_by do |album|
+    album[YEAR].to_i
+  end
+  .each do |album|
+    puts album.join(',')
+  end
 
-rock_albums = []
 # Find only the rock albums
-albums.each do |album|
-  rock_albums << album if rock_album?(album)
-end
-puts "Rock albums: #{rock_albums}"
+puts "------------------------------------------------"
+puts "Rock albums"
+CSV.read("albums.csv")[1..]
+  .select do |album|
+    album[GENRE] == "Rock"
+  end
+  .sort_by do |album|
+    album[YEAR].to_i
+  end
+  .each do |album|
+    puts album.join(',')
+  end
 
-michael_jackson_albums = []
-# Find the albums by Michael Jackson
-albums.each do |album|
-  michael_jackson_albums << album if from_michael_jackson?(album)
-end
-puts "Michael Jackson albums: #{michael_jackson_albums}"
+# Find the albums from Michael Jackson
+puts "------------------------------------------------"
+puts "Michael Jackson albums"
+CSV.read("albums.csv")[1..]
+  .select do |album|
+    album[AUTHOR] == "Michael Jackson"
+  end
+  .sort_by do |album|
+    album[YEAR].to_i
+  end
+  .each do |album|
+    puts album.join(',')
+  end
