b4d5c707376c74b19b924d21600e9b9d3d8955f4
Mario Mancilla <mariormt17@gmail.com>
2021-04-13 22:23:18 -0500

    Create the other functions and make 3 more queries about the albums

--- get-csv.rb
+++ get-csv.rb
@@ -1,10 +1,47 @@
 require "csv"
 
-def classic_jazz?(album)
-  album[2] == "Jazz" and album[3].to_i.beetwen?(1950, 1959)
-end
-
 albums = CSV.read("albums.csv")[1..]
 
-# Find jazz albums from the 50's
+def classic_jazz?(album)
+  album[2] == "Jazz" and album[3].to_i.between?(1950, 1959)
+end
 
+def classic_pop?(album)
+  album[2] == "Pop" and album[3].to_i.between?(1980, 1989)
+end
+
+def rock_album?(album)
+  album[2] == "Rock"
+end
+
+def from_michael_jackson?(album)
+  album[1] == "Michael Jackson"
+end
+
+classic_jazz_albums = []
+# Find jazz albums from the 50's
+albums.each do |album|
+  classic_jazz_albums << album if classic_jazz?(album)
+end
+puts "Jazz 50's albums: #{classic_jazz_albums}"
+
+classic_pop_albums = []
+# Find eighties pop albums
+albums.each do |album|
+  classic_pop_albums << album if classic_pop?(album)
+end
+puts "Eighties pop albums: #{classic_pop_albums}"
+
+rock_albums = []
+# Find only the rock albums
+albums.each do |album|
+  rock_albums << album if rock_album?(album)
+end
+puts "Rock albums: #{rock_albums}"
+
+michael_jackson_albums = []
+# Find the albums by Michael Jackson
+albums.each do |album|
+  michael_jackson_albums << album if from_michael_jackson?(album)
+end
+puts "Michael Jackson albums: #{michael_jackson_albums}"
